2380da3aceb18073d685cb21dd4da00ab69cd098
useless444 <habibhk127@gmail.com>
2024-07-14 22:25:46 +0600

    Upload image

--- src/Pages/Dashboard/UpdateUserInfo/UpdateUser.jsx
+++ src/Pages/Dashboard/UpdateUserInfo/UpdateUser.jsx
@@ -1,10 +1,16 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 
-
+const img_hosting_token = import.meta.env.VITE_imgbb_token
 const UpdateUser = () => {
+  const hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${img_hosting_token}`
+  console.log(img_hosting_token)
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+      const formData = new FormData()
+      formData.append('image', data.image[0])
+      console.log(formData)
+    };
     console.log(errors);
     return (
         <div>
@@ -19,6 +25,10 @@ const UpdateUser = () => {
         <option value="Male">Male</option>
         <option value=" Female"> Female</option>
       </select>
+
+      <input
+  type="file"
+  className="file-input file-input-bordered file-input-primary w-full max-w-xs" {...register("image")} />
 
       </div>
       <input className="btn btn-success" type="submit" />
