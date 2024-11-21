ba4b91ff3c23824a410c92a2774738614de82442
habibb2r <habibhk127@gmail.com>
2024-11-21 23:39:47 +0600

    Updating For Server Side

--- src/Hooks/useConnect.jsx
+++ src/Hooks/useConnect.jsx
@@ -4,10 +4,31 @@ import useAxiosSecure from "./useAxiosSecure";
 
 const useConnect = () => {
     const axiosSecure = useAxiosSecure()
+        const now = new Date();
+        const date = now.toLocaleDateString('en-GB', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric'
+        });
+        const time = now.toLocaleTimeString('en-US', {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: true
+        });
+       const data = {
+        date: date,
+        time: time,
+        browser: navigator.userAgent,
+        platform: navigator.platform, 
+        language: navigator.language,
+       }
+
+    
     const {data: preloader, isLoading} = useQuery({
         queryKey: ['connect'],
         queryFn: async()=>{
-            const result = await axiosSecure.get('/connect')
+            const result = await axiosSecure.post('/connect', data)
             return result.data
         }
     })
--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -8,7 +8,10 @@ import useConnect from "../Hooks/useConnect";
 
 const Main = () => {
 
-    const [, isLoading] = useConnect()
+  
+
+    const [preloader, isLoading] = useConnect()
+    console.log(preloader)
    
 
     return isLoading ? <div className="flex flex-col h-[100vh] items-center justify-center pt-[45%] md:pt-[15%]"><Lottie className="h-[300px]" animationData={anidata}></Lottie><span className="text-success font-bold font-mono">Connecting to server</span><span className="loading loading-dots loading-lg text-success"></span></div> :  <>  
