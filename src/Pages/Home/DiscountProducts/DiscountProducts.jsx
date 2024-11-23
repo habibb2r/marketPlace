846f441895115a77aec3ef817b99fb66ac7015b9
habibb2r <habibhk127@gmail.com>
2024-11-23 23:44:44 +0600

    Discount

--- src/Pages/Home/DiscountProducts/DiscountProducts.jsx
+++ src/Pages/Home/DiscountProducts/DiscountProducts.jsx
@@ -2,16 +2,16 @@ import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 import SlideDiscount from "./SlideDiscount";
 import ico from '../../../assets/for title/005-special-tag.png'
 import ict from '../../../assets/for title/011-shopping.png'
-import { Slide } from "react-awesome-reveal";
+import {motion} from 'framer-motion'
 
 
 const DiscountProducts = () => {
     return (
         <div className="py-10 pb-20">
             <SectionTitle title={'Offer on Products'} ico={ico} ict={ict}></SectionTitle>
-            <Slide direction="up">
+            <motion.div initial={{opacity: 0, scale: 0.8}} whileInView={{opacity: 1, scale: 1}} transition={{duration: 0.9, ease: 'easeInOut'}}>
             <SlideDiscount></SlideDiscount>
-            </Slide>
+            </motion.div>
            
         </div>
     );
