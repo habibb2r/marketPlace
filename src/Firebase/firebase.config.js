13fd4b49aed6f22f8205d188c456aa9acf1eb982
habibb2r <habibhk127@gmail.com>
2024-11-27 23:21:03 +0600

    cleaned firebase auth code

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,8 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
     
   apiKey: import.meta.env.VITE_apiKey,
@@ -16,6 +14,5 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_measurementId
 };
 
-// Initialize Firebase
+
 export const app = initializeApp(firebaseConfig);
-// export const analytics = getAnalytics(app);
